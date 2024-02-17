17e1d7c512e059d7dea2b9acac47719867185e14
Faisal Zaghloul <fzaghlou@genophore.com>
2024-02-17 11:18:21 -0800

    fix direct lighting bug

--- SDK/path-fmz/cs1230SceneConverter.cpp
+++ SDK/path-fmz/cs1230SceneConverter.cpp
@@ -146,8 +146,8 @@ SceneConverter::SceneConverter(void* scn) {
 
                 light.emission = emiss;
                 light.corner = eigen_to_vec_f(v0);
-                light.v1     = eigen_to_vec_f(v0 - v1);
-                light.v2     = eigen_to_vec_f(v0 - v2);
+                light.v1     = eigen_to_vec_f(v1-v0);
+                light.v2     = eigen_to_vec_f(v2-v0);
                 light.normal = getNormalFromVertices(v0, v1, v2);
 
                 light_found = true;
